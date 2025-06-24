c4f169a968072b33d0ba25868ed1c9bccc18358f
AohuaCheng <62255903+AohuaCheng@users.noreply.github.com>
2025-06-24 17:08:28 +0800

    add llm_test.py and update run.sh

--- MindIE-CANN/qwen/run.sh
+++ MindIE-CANN/qwen/run.sh
@@ -1,8 +1,8 @@
-curl -v http://0.0.0.0:3025/v1/chat/completions \
+curl -v http://0.0.0.0:8001/v1/chat/completions \
 -H 'Content-Type: application/json' \
 -d \
 '{
-  "model": "Qwen3-0.6B",
+  "model": "/data/models/Qwen/Qwen3-8B",
   "temperature": 0.6,
   "max_tokens": 128,
   "stream": false,
---
+++ benchmarks/ops/llm_test.py
@@ -0,0 +1,17 @@
+# ASCEND_LAUNCH_BLOCKING=1 python3 benchmarks/ops/ttest.py 
+from vllm import LLM, SamplingParams
+
+prompts = [
+    "Hello, my name is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+# The first run will take about 3-5 mins (10 MB/s) to download models
+llm = LLM(model="/data/models/Qwen/Qwen3-8B",max_model_len=1024)
+
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
